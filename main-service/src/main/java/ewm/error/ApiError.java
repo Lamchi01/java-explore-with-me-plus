1408897cd60a758c055666459d7a17980603594d
u-sly <150694587+u-sly@users.noreply.github.com>
2025-01-21 03:18:58 +0300

    Реализация базовых структур проекта. Реализация Admin контролллера для User, а так же сервис и репозиторий в части

---
+++ main-service/src/main/java/ewm/error/ApiError.java
@@ -0,0 +1,15 @@
+package ewm.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiError {
+
+    private String status;
+    private String reason;
+    private String message;
+    private String timestamp;
+
+}
