f1df2cdc29515bdabc4b3dea042e9156c9867f7a
Daniil Andreev <daniiland9@yandex.ru>
2025-01-21 19:19:06 +0300

    feat: add EventShortDto

---
+++ main-service/src/main/java/ewm/event/dto/EventFullDto.java
@@ -0,0 +1,27 @@
+package ewm.event.dto;
+
+import ewm.category.dto.CategoryDto;
+import ewm.user.dto.UserShortDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EventFullDto {
+    private String annotation;
+    private CategoryDto category;
+    private Long confirmedRequests;
+    private String createdOn;
+    private String description;
+    private String eventDate;
+    private Long id;
+    private UserShortDto initiator;
+    private Location location;
+    private Boolean paid;
+    private Long participantLimit;
+    private String publishedOn;
+    private Boolean requestModeration;
+    private String state;
+    private String title;
+    private Long views;
+}
