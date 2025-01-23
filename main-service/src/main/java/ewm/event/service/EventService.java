18158033b3dee2c66447e3c10afcf8e811500794
Lamchi01 <sonic24072003@yandex.ru>
2025-01-23 03:20:28 +0300

    Промежуточный коммит, с доработкой после реализации private

--- main-service/src/main/java/ewm/event/service/EventService.java
+++ main-service/src/main/java/ewm/event/service/EventService.java
@@ -1,4 +1,14 @@
 package ewm.event.service;
 
+import ewm.event.dto.EventFullDto;
+import ewm.event.dto.EventShortDto;
+import ewm.event.dto.ReqParam;
+
+import java.util.List;
+
 public interface EventService {
+
+    List<EventShortDto> publicGetAllEvents(ReqParam reqParam);
+
+    EventFullDto publicGetEvent(long id);
 }
--- main-service/src/main/java/ewm/event/service/EventServiceImpl.java
+++ main-service/src/main/java/ewm/event/service/EventServiceImpl.java
@@ -1,4 +1,76 @@
 package ewm.event.service;
 
-public class EventServiceImpl {
+import ewm.ParamDto;
+import ewm.ViewStats;
+import ewm.client.RestStatClient;
+import ewm.event.dto.EventFullDto;
+import ewm.event.dto.EventShortDto;
+import ewm.event.dto.ReqParam;
+import ewm.event.mapper.EventMapper;
+import ewm.event.model.Event;
+import ewm.event.repository.EventRepository;
+import ewm.exception.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventServiceImpl implements EventService {
+    private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
+    private final RestStatClient statClient;
+
+    @Override
+    public List<EventShortDto> publicGetAllEvents(ReqParam reqParam) {
+        Pageable pageable = PageRequest.of(reqParam.getFrom(), reqParam.getSize());
+        List<EventShortDto> eventShortDtos = eventMapper.toEventShortDto(eventRepository.findEvents(
+                reqParam.getText(),
+                reqParam.getCategories(),
+                reqParam.getPaid(),
+                reqParam.getRangeStart(),
+                reqParam.getRangeEnd(),
+                reqParam.getOnlyAvailable(),
+                pageable
+        ));
+        List<EventShortDto> addedViews = eventShortDtos.stream().map(this::addViews).toList();
+        return switch (reqParam.getSort()) {
+            case EVENT_DATE -> addedViews.stream().sorted(Comparator.comparing(EventShortDto::getEventDate)).toList();
+            case VIEWS -> addedViews.stream().sorted(Comparator.comparing(EventShortDto::getViews)).toList();
+        };
+    }
+
+    @Override
+    public EventFullDto publicGetEvent(long id) {
+        return addViews(eventMapper.toEventFullDto(eventRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Event.class, "Событие c ID - " + id + ", не найдено."))));
+    }
+
+    private EventFullDto addViews(EventFullDto eventDto) {
+        List<String> gettingUris = new ArrayList<>();
+        gettingUris.add("/events/" + eventDto.getId());
+        ParamDto paramDto = new ParamDto(LocalDateTime.now().minusYears(1), LocalDateTime.now(), gettingUris, true);
+        Long views = statClient.getStat(paramDto)
+                .stream()
+                .map(ViewStats::getHits)
+                .reduce(0L, Long::sum);
+        eventDto.setViews(views);
+        return eventDto;
+    }
+
+    private EventShortDto addViews(EventShortDto eventShortDto) {
+        List<String> gettingUris = new ArrayList<>();
+        gettingUris.add("/events/" + eventShortDto.getId());
+        ParamDto paramDto = new ParamDto(LocalDateTime.now().minusYears(1), LocalDateTime.now(), gettingUris, true);
+        Long views = statClient.getStat(paramDto)
+                .stream().map(ViewStats::getHits).reduce(0L, Long::sum);
+        eventShortDto.setViews(views);
+        return eventShortDto;
+    }
 }
