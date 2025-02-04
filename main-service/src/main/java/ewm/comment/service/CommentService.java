5c7e2949d3f36335e0dcf18981849d3e43e8ae19
uncle-sly <154626220+uncle-sly@users.noreply.github.com>
2025-02-04 13:52:02 +0300

    Feature admin (#24)
    * Админская часть относительно комментариев
    AdminCommentController и т.д.
    
    * Админская часть относительно комментариев
    AdminCommentController и т.д.
    
    * Comment
    поправил имена методов и маппер
    
    * Comment
    fix комментариев
    
    * Добавлены тесты для feature для админской части ендпоинтов
    
    ---------
    
    Co-authored-by: u-sly <150694587+u-sly@users.noreply.github.com>

--- main-service/src/main/java/ewm/comment/service/CommentService.java
+++ main-service/src/main/java/ewm/comment/service/CommentService.java
@@ -2,11 +2,20 @@ package ewm.comment.service;
 
 import ewm.comment.dto.CommentDto;
 import ewm.comment.dto.InputCommentDto;
+import ewm.comment.dto.UpdateCommentDto;
 
 public interface CommentService {
-    CommentDto addComment(Long userId, Long eventId, InputCommentDto inputCommentDto);
 
-    CommentDto updateComment(Long userId, Long commentId, InputCommentDto inputCommentDto);
+    CommentDto add(Long userId, Long eventId, InputCommentDto inputCommentDto);
 
-    void deleteComment(Long userId, Long commentId);
+    CommentDto add(Long adminId, Long eventId, CommentDto commentDto);
+
+    CommentDto update(Long userId, Long commentId, InputCommentDto inputCommentDto);
+
+    CommentDto update(Long id, UpdateCommentDto updateCommentDto);
+
+    void delete(Long id);
+
+    void delete(Long userId, Long commentId);
+
 }
